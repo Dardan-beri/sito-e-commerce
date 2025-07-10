f949514c19142cc71a5722f0485c937e79656386
Dardanmat <dardanmatias@gmail.com>
2025-07-10 17:24:21 +0200

    Aggiunti altri campi user ad admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,70 @@
 from django.contrib import admin
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
 
 class CustomUserAdmin(UserAdmin):
+    # Form per l'aggiunta e la modifica degli utenti
     add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["username", "email", "is_staff"]
+
+    # Campi da visualizzare nella lista utenti
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active'
+    ]
+
+    # Filtri disponibili nella sidebar
+    list_filter = [
+        'is_staff',
+        'is_active'
+    ]
+
+    # Configurazione dei fieldset per la modifica utente
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'address')}),
+        ('Permissions', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions'
+            ),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Configurazione dei fieldset per la creazione utente
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username',
+                'email',
+                'first_name',
+                'last_name',
+                'address',
+                'password1',
+                'password2',
+                'is_staff',
+                'is_active'
+            )}
+         ),
+    )
+
+    # Campi per cui è disponibile la ricerca
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+    # Ordinamento predefinito
+    ordering = ('username',)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+from django.contrib.auth.forms import UserChangeForm
 
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
@@ -10,3 +11,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'first_name', 'last_name', 'address', 'password1', 'password2')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = '__all__'
